bf2f15fcac7282776b90df23ab8877a785f1d872
Ryan Tran <rhardytran@gmail.com>
2024-02-16 16:59:16 -0800

    remove section-03

--- devop-bootcamp/section-03/04-ec2/main.tf
+++
@@ -1,8 +0,0 @@
-# Create an EC2 instance
-resource "aws_instance" "server" {
-  ami = "ami-06ec8443c2a35b0ba"
-  instance_type = "t2.micro"
-  tags = {
-    "Name" = "server"
-  }
-}
--- devop-bootcamp/section-03/06-variable-config/prod.tfvars
+++
@@ -1,4 +0,0 @@
-vpc_cidr_block = "20.0.0.0/16"
-web_subnet = "20.0.0.0/24"
-subnet_zone = "us-east-1b"
-main_vpc_name = "Main VPC"
